c521e59e5734daa96406b35cbce0fe6c7f2491b8
Franck COLOMBO <franck.colombo@cnav.fr>
2021-06-29 10:57:44 +0200

    display update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,23 @@
 <template lang="pug">
 .container.m-3
-  h1.text-center Apy calculator EGLD
+  h1.text-center Apy calculator 
   .container(class="flex md:space-x-4 flex-col items-center md:justify-around md:flex-row").mx-auto
     label.block
-      | Amount
+      | Amount staked
       input(type="number" v-model="data.amount" ).block.mt-1
     label.block
-      | APR
-      input(type="number"  v-model="data.apr").block.mt-1
+      | APR 
+      span.text-xs (see on your service provider)
+      input(type="number",  v-model="data.apr", step="0.01").block.mt-1
     label.block
-      | Duration
+      | Duration 
+      span.text-xs (days) 
       input(type="number"  v-model="data.duration").block.mt-1
-    
-  p.mt-5.text-2xl.w-full.font-bold.text-center result
-    span.block {{apyCalc()}} EGLD
-  p.mt-5.text-xl.w-full.font-bold.text-center ROI
-    span.block {{apyCalc() - data.amount}} EGLD
+  .container.flex.items-center.justify-evenly
+    p.mt-5.text-2xl.font-bold Total after {{data.duration}} day(s)
+      span.block {{apyCalc().toFixed(5)}} EGLD
+    p.mt-5.text-xl.font-bold ROI
+      span.block {{(apyCalc() - data.amount).toFixed(5)}} EGLD
 </template>
 
 <script lang="ts">
